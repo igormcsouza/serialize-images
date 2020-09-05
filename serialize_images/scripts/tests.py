777dfedd918ef7a5b1c03255a43dd3db017959c5
Igor Souza <igormcsouza@gmail.com>
2020-09-05 00:04:38 -0300

    adding coverage

--- serialize_images/scripts/tests.py
+++ serialize_images/scripts/tests.py
@@ -2,4 +2,8 @@ import os
 
 
 def run_tests():
-    os.system('pytest -v')
+    os.system('pytest -v --cov=serialize_images')
+
+def open_web_coverage():
+    os.system('coverage html')
+    os.system(f"chrome {os.path.join('htmlcov', 'index.html')}")
